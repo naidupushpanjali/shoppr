65380e518c886766ddfb5ac9882c489f2108b759
pushpa <pushpa@Admin-003-MBP.local>
2021-02-24 21:44:10 +0530

    Shopper React initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import Skeleton from "@material-ui/lab/Skeleton";
 import React, { useState, useEffect, Suspense } from "react";
+import ImagePlaceholder from "./images/image_placeholder.png";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { makeStyles } from "@material-ui/core/styles";
+import "./App.css";
+import Grow from "@material-ui/core/Grow";
 
 const ImageComponent = React.lazy(() => import("./ImageComponent"));
 
+const useStyles = makeStyles((theme) => ({
+	brand_information: {
+		marginLeft: "5%",
+		[theme.breakpoints.down("xs")]: {
+			marginLeft: 0,
+		},
+	},
+	brand_description: {
+		width: 420,
+		fontSize: 20,
+		fontFamily: "Lato-Regular",
+		[theme.breakpoints.down("xs")]: {
+			fontSize: 15,
+			padding: "0px 15px",
+			marginTop: 15,
+			width: "100%",
+		},
+	},
+	wrapper: {
+		display: "flex",
+		margin: "30px 0px",
+		[theme.breakpoints.down("xs")]: {
+			display: "block",
+			margin: "20px auto",
+			textAlign: "center",
+			marginBottom: 10,
+		},
+	},
+	h1: {
+		fontFamily: "Lato-Bold",
+		[theme.breakpoints.down("xs")]: {
+			fontSize: 30,
+		},
+	},
+}));
+
 const List = () => {
 	const [listItems, setListItems] = useState([]);
+	const [header, setHeader] = useState([]);
 	const [isFetching, setIsFetching] = useState(true);
 	const [page_no, setPageno] = useState(1);
 	const [data, setData] = useState(true);
 	let fetching = false;
+	const classes = useStyles();
 
 	useEffect(() => {
 		if (data) {
@@ -20,16 +62,16 @@ const List = () => {
 	const fetchData = async () => {
 		setTimeout(async () => {
 			const result = await fetch(
-				`https://beingfoxy.publshr.io/wp-json/shoppr/v1/get_instagram_posts?page_no=${page_no}&per_page=10`
+				`https://demo.shoppr.io/wp-json/egram/v1/get_instagram_posts_with_header?page_no=${page_no}&per_page=10`
 			);
 			const data = await result.json();
 			setPageno(page_no + 1);
-			// debugger;
-			if (data.instagram_post_details.length > 0) {
+			if (data.content.instagram_post_details.length > 0) {
 				setData(true);
 				setListItems(() => {
-					return [...listItems, ...data.instagram_post_details];
+					return [...listItems, ...data.content.instagram_post_details];
 				});
+				setHeader(data.header.app_config);
 				setIsFetching(true);
 			} else {
 				setData(false);
@@ -48,22 +90,59 @@ const List = () => {
 		setIsFetching(true);
 	};
 
+	const handleAddDefaultSrc = (e) => {
+		e.target.src = ImagePlaceholder;
+	};
+
 	return (
 		<div className="container">
-			<div className="row">
-				<div className="col-sm-12">
+			<div className="row justify-content-center">
+				<div className="col-sm-10">
+					<div className="row justify-content-center">
+						<Suspense
+							fallback={<Skeleton animation="wave" width={210} height={210} />}
+						>
+							<div className={classes.wrapper}>
+								<ImageComponent
+									src={header.brand_logo}
+									section="header"
+									onAddDefaultSrc={handleAddDefaultSrc}
+								/>
+
+								<div className={classes.brand_information}>
+									<h1 className={classes.h1}>{header.brand_name}</h1>
+									<p className={classes.brand_description}>
+										{header.brand_description}
+									</p>
+								</div>
+							</div>
+						</Suspense>
+					</div>
+				</div>
+			</div>
+			<div className="row justify-content-center">
+				<div className="col-sm-10">
 					<ResponsiveMasonry
 						columnsCountBreakPoints={{ 350: 3, 750: 3, 900: 3 }}
 					>
-						<Masonry columnsCount={3} gutter="15px">
+						<Masonry columnsCount={3} gutter="15px" className="masonary">
 							{listItems.map((listItem) => (
 								<Suspense
-									key={listItem.id}
+									key={listItem.post_details.ID}
 									fallback={
 										<Skeleton animation="wave" width={210} height={210} />
 									}
 								>
-									<ImageComponent src={listItem.post_meta.social_post_image} />
+									<ImageComponent
+										src={
+											listItem.post_meta.social_post_image
+												? listItem.post_meta.social_post_image
+												: ""
+										}
+										section="body"
+										listItem={listItem}
+										onAddDefaultSrc={handleAddDefaultSrc}
+									/>
 								</Suspense>
 							))}
 						</Masonry>
--- src/ImageComponent.jsx
+++ src/ImageComponent.jsx
@@ -1,8 +1,74 @@
 import React from "react";
-import imageFile from "./img_avatar.png";
+import { makeStyles } from "@material-ui/core/styles";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import Grow from "@material-ui/core/Grow";
 
-const ImageComponent = ({ src }) => {
-	return <img src={src} alt="No Post" style={{ width: "100%" }} />;
+const useStyles = makeStyles((theme) => ({
+	brand_image: {
+		borderRadius: "50%",
+		[theme.breakpoints.down("xs")]: {
+			width: "23%",
+		},
+	},
+	position: {
+		position: "absolute",
+		right: 20,
+		top: 20,
+		color: "#fff",
+	},
+}));
+
+const ImageComponent = ({ src, onAddDefaultSrc, section, listItem }) => {
+	const classes = useStyles();
+
+	return section === "header" ? (
+		<a href="!#">
+			<img
+				src={src}
+				alt=""
+				onError={onAddDefaultSrc}
+				className={classes.brand_image}
+			/>
+		</a>
+	) : listItem.post_meta.has_link === "true" ? (
+		<Grow
+			in={true}
+			style={{ transformOrigin: "0 0 0" }}
+			{...(true ? { timeout: 1000 } : {})}
+		>
+			<a
+				href={listItem.post_meta.post_ext_link}
+				target="_blank"
+				rel="noreferrer"
+			>
+				<img
+					src={src}
+					alt="Post"
+					style={{ width: "100%" }}
+					onError={onAddDefaultSrc}
+				/>
+			</a>
+		</Grow>
+	) : (
+		<Grow in={true} {...(true ? { timeout: 1000 } : {})}>
+			<>
+				<img
+					src={src}
+					alt="Post"
+					style={{ width: "100%" }}
+					onError={onAddDefaultSrc}
+				/>
+				{listItem.post_meta.feed_type === "carousel" ? (
+					<PhotoLibraryIcon className={classes.position} fontSize="large" />
+				) : listItem.post_meta.feed_type === "video" ? (
+					<VideocamIcon className={classes.position} fontSize="large" />
+				) : (
+					""
+				)}
+			</>
+		</Grow>
+	);
 };
 
 export default ImageComponent;
